b560175f8ee37b915b790446d6c723cba7e10662
SherylNyawira <sherylnyawira@gmail.com>
2025-01-20 22:47:42 +0300

    learning some ES6+ (ternaty operators)

---
+++ ternary_operators.js
@@ -0,0 +1,7 @@
+const age = 18;
+const canVote = age >= 18 ? 'Yes, you can vote' : 'No, you cannot vote';
+console.log(canVote); // Outputs: "Yes, you can vote"
+ 
+const a = 10;
+const b = 20;
+const result = a > b ? console.log('a is greater') : console.log('b is greater');
